206d677f347b45fd208561ac33c99779ad43c828
Gleb Bulygin <gleb.a.bulygin@gmail.com>
2025-09-12 23:18:06 +0300

    :white_check_mark: 1.7, 1.8, 1.9

--- part1/statistics/src/Statistics.jsx
+++ part1/statistics/src/Statistics.jsx
@@ -5,15 +5,25 @@ const Statistics = ({ props }) => {
 	const all = good + neutral + bad;
 	const avg = all === 0 ? 0 : (good - bad) / all;
 	const pos = all === 0 ? 0 : (good / all) * 100;
+	let output;
+	if (all === 0) {
+		output = <div>No Feedback Given</div>;
+	} else {
+		output = (
+			<div style={{ textAlign: "left" }}>
+				<p>Good: {good}</p>
+				<p>Neutral: {neutral}</p>
+				<p>Bad: {bad}</p>
+				<p>All: {all}</p>
+				<p>Average: {avg.toFixed(2)}</p>
+				<p>Positive: {pos.toFixed(2)}%</p>
+			</div>
+		);
+	}
 	return (
 		<div>
 			<h1>Statistics</h1>
-			<p>Good: {good}</p>
-			<p>Neutral: {neutral}</p>
-			<p>Bad: {bad}</p>
-			<p>All: {all}</p>
-			<p>Average: {avg.toFixed(2)}</p>
-			<p>Positive: {pos.toFixed(2)}%</p>
+			{output}
 		</div>
 	);
 };
