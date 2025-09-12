67d86904ca6a27a53c71abb4bc1b0ad542caa41e
Gleb Bulygin <gleb.a.bulygin@gmail.com>
2025-09-12 23:06:00 +0300

    :white_check_mark: 1.7

--- part1/statistics/src/Statistics.jsx
+++ part1/statistics/src/Statistics.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 
 const Statistics = ({ props }) => {
-	let { onClick, text, fieldName } = props;
+	const { good, neutral, bad } = props;
+	const all = good + neutral + bad;
+	const avg = all === 0 ? 0 : (good - bad) / all;
+	const pos = all === 0 ? 0 : (good / all) * 100;
 	return (
 		<div>
 			<h1>Statistics</h1>
-			<p>Good: {props.good}</p>
-			<p>Neutral: {props.neutral}</p>
-			<p>Bad: {props.bad}</p>
+			<p>Good: {good}</p>
+			<p>Neutral: {neutral}</p>
+			<p>Bad: {bad}</p>
+			<p>All: {all}</p>
+			<p>Average: {avg.toFixed(2)}</p>
+			<p>Positive: {pos.toFixed(2)}%</p>
 		</div>
 	);
 };
