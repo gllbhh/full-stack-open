49bb250121709fd897f2868b6411828cb30c2791
Gleb Bulygin <gleb.a.bulygin@gmail.com>
2025-12-17 11:12:21 +0200

    2.3, update reduce method in Total component

--- part2/react-app/src/Total.jsx
+++ part2/react-app/src/Total.jsx
@@ -3,8 +3,12 @@ import React from "react";
 export default function Header({ props }) {
 	const exercises = props.parts.map((e) => e.exercises);
 	console.log(exercises);
-	const sum = exercises.reduce((s, e) => s + e, 0);
-	console.log(sum);
+	// use array.reduce() method with an arrow function to calculate sum
+	const sum = exercises.reduce((s, e) => {
+		console.log("accumulator: ", s, "| current value: ", e);
+		return s + e;
+	}, 0); // ,0 sets initail value to 0
+	console.log("total: ", sum);
 	return (
 		<p>
 			<b>Total of {sum} exercises</b>
