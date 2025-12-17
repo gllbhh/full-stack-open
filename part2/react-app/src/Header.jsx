5b44d0776b372a5d1e2c90a3b77461b68a3c277a
Gleb Bulygin <gleb.a.bulygin@gmail.com>
2025-12-17 11:22:03 +0200

    2.4 update App.jsx to take and display an array of courses

--- part2/react-app/src/Header.jsx
+++ part2/react-app/src/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = ({ header }) => {
 	console.log("Header loaded");
 	console.log("Course name: ", header);
-	return <h1>{header}</h1>;
+	return <h2>{header}</h2>;
 };
 
 export default Header;
